4da982f5a5dadb8f7b78bba907945a1510d93d6a
liumengjin <liumengjin@bytedance.com>
2023-08-14 20:05:30 +0800

    example01

--- example01/src/main.rs
+++ example01/src/main.rs
@@ -1,10 +1,11 @@
 use std::os::raw::c_int;
 
+#[link(name="callee")]
 extern "C" {
-    fn add(a: c_int, b: c_int) -> c_int;
+    fn sum(a: c_int, b: c_int) -> c_int;
 }
 
 fn main() {
-    let k = unsafe { add(2, 4) };
-    assert!(k == 6);
+    let k = unsafe { sum(2, 4) };
+    println!("sum result is {}",k);
 }
